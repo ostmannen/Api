134dee2de0cd311c0106f4f8ea3ee4cc842c664f
ostmannen <60339614+ostmannen@users.noreply.github.com>
2021-02-23 12:36:32 +0100

    first commit

---
+++ hej/Pokemon.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace hej
+{
+    public class Pokemon
+    {
+        public string name;
+        public int weight;
+        public int height;
+        public int base_experience;
+    }
+}
